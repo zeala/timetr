14deb93028dc993702573b85fcb9bdd0e8a2cb50
Elena <lena@soapyfrog.com>
2015-12-08 11:55:28 +0000

    work in progress

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,19 @@
 class CompaniesController < ApplicationController
     def index
-        @hello = 'Hello :)'
         @companies = Company.all
 
     end
 
     def show
         @company = Company.find(params[:id])
+        respond_to do |format|
+            format.html # show html file
+            format.xml {render xml: @company }
+            format.json { render json: @company}
+        end
     end
+
+  def new
+      @company = Company.new
+  end
 end
