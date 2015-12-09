06455566f4629d5ec034f51d616014d89f1ac883
Elena <lena@soapyfrog.com>
2015-12-09 10:52:05 +0000

    validation for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,8 +17,11 @@ class ProjectsController < ApplicationController
 
     def create
       @project = Project.new(params[:project].permit(:name, :company_id, :slug, :default_rate))
-      @project.save
-      flash[:notice] = "New project entry is created"
-      redirect_to @project
+      if @project.save
+        flash[:notice] = "New project entry is created"
+        redirect_to @project
+      else
+        render 'new'
+      end
     end
 end
